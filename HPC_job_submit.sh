124619c4ca8fe930c787033d217cc8ff778aa314
GreyAudron <mikhail.schee@mail.utoronto.ca>
2023-10-03 17:18:54 -0400

    Added lines to ensure pulling from git works

--- HPC_job_submit.sh
+++ HPC_job_submit.sh
@@ -33,6 +33,8 @@ fi
 LANCEUR="HPC_lanceur.slrm"
 
 # Pull the most recent changes from git
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_GH_23
 git pull
 
 ###############################################################################
