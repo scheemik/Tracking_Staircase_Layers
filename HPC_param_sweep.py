f17d0a5b781f07822dc7b53753a1374f5d4b3664
GreyAudron <mikhail.schee@mail.utoronto.ca>
2023-10-27 16:13:23 -0400

    Sweep for ITP2 at ell = 10

--- HPC_param_sweep.py
+++ HPC_param_sweep.py
@@ -38,7 +38,7 @@ from datetime import datetime
 # os.environ['MPLCONFIGDIR'] = 'scratch/n/ngrisoua/mschee/.config/matplotlib'
 
 # Title
-this_plot_title = 'BGR0708'
+this_plot_title = 'ITP2_ell_10'# 'BGR0708'
 
 # Get MPI variables set up
 comm = MPI.COMM_WORLD
@@ -67,6 +67,7 @@ BGR04 = {'BGR04':'all'}
 BGR0506 = {'BGR0506':'all'}
 BGR0607 = {'BGR0607':'all'}
 BGR0708 = {'BGR0708':'all'}
+ITP2 = {'ITP_002':'all'}
 
 ################################################################################
 # Create data filtering objects
@@ -87,7 +88,8 @@ dfs1 = ahf.Data_Filters(min_press=this_min_press)
 # ds_this_BGR = ahf.Data_Set(BGR04, dfs_all)
 # ds_this_BGR = ahf.Data_Set(BGR0506, dfs_all)
 # ds_this_BGR = ahf.Data_Set(BGR0607, dfs_all)
-ds_this_BGR = ahf.Data_Set(BGR0708, dfs_all)
+# ds_this_BGR = ahf.Data_Set(BGR0708, dfs_all)
+ds_this_BGR = ahf.Data_Set(ITP2, dfs1)
 
 ################################################################################
 # Create profile filtering objects
@@ -97,11 +99,16 @@ ds_this_BGR = ahf.Data_Set(BGR0708, dfs_all)
 pfs_0 = ahf.Profile_Filters()
 pfs_1 = ahf.Profile_Filters(every_nth_row=4)
 
+test_p_range = [400,200]
+pfs_ell  = ahf.Profile_Filters(p_range=test_p_range, m_avg_win=10)
+
 ################################################################################
 
 ## Preclustered
-pfs_this_BGR = pfs_0
+# pfs_this_BGR = pfs_0
 # pfs_this_BGR = pfs_1
+
+pfs_this_BGR = pfs_ell
 
 ################################################################################
 ### Figures
