9f7aead8fb87c51aa0c4dbe819b90d47592cd4b1
GreyAudron <mikhail.schee@mail.utoronto.ca>
2024-03-01 15:53:42 -0500

    Running BGR1415 ps on Niagara

--- HPC_param_sweep.py
+++ HPC_param_sweep.py
@@ -38,7 +38,7 @@ from datetime import datetime
 # os.environ['MPLCONFIGDIR'] = 'scratch/n/ngrisoua/mschee/.config/matplotlib'
 
 # Title
-this_plot_title = 'new_BGR1314'
+this_plot_title = 'new_BGR1415'
 
 # Get MPI variables set up
 comm = MPI.COMM_WORLD
@@ -116,8 +116,8 @@ if rank%rf == 0:
     # ds_this_BGR = ahf.Data_Set(BGR1011, dfs_all)
     # ds_this_BGR = ahf.Data_Set(BGR1112, dfs_all)
     # ds_this_BGR = ahf.Data_Set(BGR1213, dfs_all)
-    ds_this_BGR = ahf.Data_Set(BGR1314, dfs_all)
-    # ds_this_BGR = ahf.Data_Set(BGR1415, dfs_all)
+    # ds_this_BGR = ahf.Data_Set(BGR1314, dfs_all)
+    ds_this_BGR = ahf.Data_Set(BGR1415, dfs_all)
     # ds_this_BGR = ahf.Data_Set(BGR1516, dfs_all)
     # ds_this_BGR = ahf.Data_Set(BGR1617, dfs_all)
     # ds_this_BGR = ahf.Data_Set(BGR1718, dfs_all)
